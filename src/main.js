303c6a1f15ba70a8821a241e8ec7c0970b7eb82b
KovacsBars <kovibarsi@gmail.com>
2025-02-03 15:17:36 +0100

    .

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
+import * as readline from 'node:readline/promises';
+import { stdin as input, stdout as output } from 'node:process';
+import { open } from 'node:fs/promises';
+
 const { osszead, kivon, szoroz, eloszt } = require('./szamologep.js');
 var isEven = require('is-even');
 
-const a = 2;
+const rl = readline.createInterface({ input, output });
 
-const b = 1;
+let answer = await rl.question('Kérem az első számot: ');
+let a = parseInt(answer);
+answer = await rl.question('Kérem a második számot: ');
+let b = parseInt(answer);
 
 console.log(`Összeadás: ${a} + ${b} = ${osszead(a, b)} és ${isEven(osszead(a, b)) ? 'páros' : 'páratlan'}`);
 console.log(`Kivonás: ${a} - ${b} = ${kivon(a, b)} és ${isEven(kivon(a, b)) ? 'páros' : 'páratlan'}`);
 console.log(`Szorzás: ${a} * ${b} = ${szoroz(a, b)} és ${isEven(szoroz(a, b)) ? 'páros' : 'páratlan'}`);
 console.log(`Osztás: ${a} / ${b} = ${eloszt(a, b)} és ${isEven(eloszt(a, b)) ? 'páros' : 'páratlan'}`);
+
+rl.close();
+
+let filehandle = null;
+try {
+    filehandle = await open('eredmeny.txt', 'w');
+    await filehandle.writeFile(`Összeadás: ${a} + ${b} = ${osszead(a, b)} és ${isEven(osszead(a, b)) ? 'páros' : 'páratlan'}\n`);
+} finally {
+    await filehndle?.close();
+}
 
